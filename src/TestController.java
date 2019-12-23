54b6b2a5fb98403d29f17b91407f39906a71b0a4
liudi <470382612@qq.com>
2019-12-23 10:06:36 +0800

    ld版本

--- src/TestController.java
+++ src/TestController.java
@@ -3,9 +3,9 @@ public class TestController {
      *ld测试
      */
     public static void main(String[] args) {
-        System.out.println("第一次测试");
-        System.out.println("uat第一次测试");
-        System.out.println("uat_ld第一次测试");
-        System.out.println("uat第二次测试");
+
+        System.out.println("ga");
+
+        System.out.println("ga");
     }
 }
