b25a804c763a06fd0e994622febcdd46f44a1db8
liudi <470382612@qq.com>
2019-12-23 19:05:47 +0800

    Merge branch 'uat_ld' of G:\liudi\gitDemo with conflicts.

--- src/TestController.java
+++ src/TestController.java
@@ -1,11 +1,10 @@
 public class TestController {
     /*
-     *ld测试
+     *master版本
      */
     public static void main(String[] args) {
-
-        System.out.println("ga");
-
-        System.out.println("ga");
+        System.out.println("第一次测试");
+        System.out.println("uat第一次测试");
+        System.out.println("uat第二次测试");
     }
 }
--- src/TestDemo.java
+++
@@ -1,3 +0,0 @@
-public class TestDemo {
-    private String name;
-}
